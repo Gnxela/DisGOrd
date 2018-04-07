25423dd01a06c04db1c6585667b696fbb42fe0d1
Gnxela <qawswedfrgtt@gmail.com>
2018-04-07 14:11:20 +0100

    Refarcored code to allow for checking of permissions other than admin.

--- DisGOrd.go
+++ DisGOrd.go
@@ -160,12 +160,16 @@ func onMessage(session *discordgo.Session, message *discordgo.MessageCreate) {
 }
 
 func checkAdmin(session *discordgo.Session, channelID string, userID string) (bool){
+	return checkPermission(session, discordgo.PermissionAdministrator, channelID, userID)
+}
+
+func checkPermission(session *discordgo.Session, permission int, channelID string, userID string) (bool) {
 	permissions, err := session.State.UserChannelPermissions(userID, channelID)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return false
 	}
-	return permissions&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator
+	return permissions&permission == permission
 }
 
 func load(bot *common.Bot, session *discordgo.Session, message *discordgo.MessageCreate) {
