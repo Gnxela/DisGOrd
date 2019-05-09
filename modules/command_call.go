0802f683f9113c849b784cf1d9302757c778e641
Gnxela <qawswedfrgtt@gmail.com>
2019-05-09 22:06:52 +0100

    Made call more responsive.

--- modules/command_call.go
+++ modules/command_call.go
@@ -21,6 +21,8 @@ var (
 func createLexer() {
 	sequences := []*common.Sequence{common.CreateSequence(&common.AbsoluteToken{"!call"}, &common.AbsoluteToken{"add"}, &common.StringToken{}, &common.StringToken{}),
 		common.CreateSequence(&common.AbsoluteToken{"!call"}, &common.AbsoluteToken{"remove"}, &common.StringToken{}),
+		common.CreateSequence(&common.AbsoluteToken{"!call"}, &common.AbsoluteToken{"list"}),
+		common.CreateSequence(&common.AbsoluteToken{"!call"}),
 	}
 	for alias, _ := range config.Roles {
 		sequences = append(sequences, common.CreateSequence(&common.AbsoluteToken{"!" + alias}))
@@ -42,7 +44,7 @@ func Unload() {
 }
 
 func GetData(bot *common.Bot) common.Data {
-	return common.Data{"Call", "@'s all online users with specific roles.", "!call add [alias] [RollID]. !call remove [alias]. ![alias]", common.PRIORITY_LOW}
+	return common.Data{"Call", "@'s all online users with specific roles.", "!call list. !call add [alias] [RollID]. !call remove [alias]. ![alias]", common.PRIORITY_LOW}
 }
 
 func Fire(bot *common.Bot, session *discordgo.Session, message *discordgo.MessageCreate) bool {
@@ -56,6 +58,10 @@ func Fire(bot *common.Bot, session *discordgo.Session, message *discordgo.Messag
 		delete(config.Roles, values[2].(string))
 		createLexer()
 		session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("<@%s>, role removed.", message.Author.ID))
+	case 2: //List
+		session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("<@%s>, %v", message.Author.ID, config.Roles))
+	case 3: //Invalid
+		session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("<@%s>, %s", message.Author.ID, GetData(bot).Usage))
 	default: //Call role
 		alias := message.Content[1:]
 		roleID := config.Roles[alias]
